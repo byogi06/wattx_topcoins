a384f788b3e7833a0b11ea022e6f2a7a9427dfc8
Yogesh Bhagat <45122979+byogi06@users.noreply.github.com>
2021-11-21 23:40:49 +0100

    Update topcoins_config.py
    container name format changed

--- topcoins_app/topcoins_config.py
+++ topcoins_app/topcoins_config.py
@@ -1,4 +1,4 @@
 
 #TopCoin service config parameters
-RANK_TOPCOIN_URL = 'http://rank_app_con:5001'
-PRICE_TOPCOIN_URL = 'http://price_app_con:5002'
+RANK_TOPCOIN_URL = 'http://rank-app-con:5001'
+PRICE_TOPCOIN_URL = 'http://price-app-con:5002'
